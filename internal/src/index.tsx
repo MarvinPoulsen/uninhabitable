206baa22ac96a87b62f0ca97efd165a806ad382c
Marvin <marpo@lolland.dk>
2025-02-11 15:06:51 +0100

    update date-fns

--- internal/src/index.tsx
+++ internal/src/index.tsx
@@ -5,4 +5,8 @@ import './index.scss';
 
 const MOUNT_NODE = document.getElementById('root');
 const root = createRoot(MOUNT_NODE as Element);
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
